ea41e44d261fe80119ec090a6b3c72edfcd78f6f
dromero@bsch.bancroftschool.org <dromero@bsch.bancroftschool.org@192.168.9.202>
2024-01-19 14:53:02 -0500

    updated version with inverted matrices

--- MoveLimb.groovy
+++ MoveLimb.groovy
@@ -28,35 +28,21 @@ DHParameterKinematics leg0 = base.getAllDHChains().get(0)
 double zLift=25
 println "Start from where the arm already is and move it from there with absolute location"
 TransformNR current = leg0.calcHome()
+TransformNR homereference = current.copy()
 println current
 current.translateZ(zLift);
-println current
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)// wait for the legs to fully arrive
+TransformNR ofset = current.times(homereference.inverse())
+for(double i=0;i<1;i+=0.01) {
+	TransformNR tmp = ofset.scale(i);
+	TransformNR step = homereference.times(tmp)
+	println step
+	leg0.setDesiredTaskSpaceTransform(step,  2.0);
+	ThreadUtil.wait(10)// wait for the legs to fully arrive
+}
 
 println "and reset it"
 current.translateZ(-zLift);
 leg0.setDesiredTaskSpaceTransform(current,  2.0);
 ThreadUtil.wait(2000)// wait for the legs to fully arrive
-
-current.translateX(zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)
-println "wait for the legs to fully arrive"
-println "and reset it"
-current.translateX(-zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)
-println " wait for the legs to fully arrive"
-
-current.translateY(-zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)
-println " wait for the legs to fully arrive"
-println "and reset it"
-current.translateY(zLift);
-leg0.setDesiredTaskSpaceTransform(current,  2.0);
-ThreadUtil.wait(2000)
-println " wait for the legs to fully arrive"
 
 return null;
