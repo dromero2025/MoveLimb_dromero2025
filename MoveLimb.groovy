bad4c76282bb6a2d87528b3e6ac59c7e7442fc29
amand <amand@MethMotor.search.charter.com>
2024-01-24 22:38:18 -0500

    made it move along the global coordinate z

--- MoveLimb.groovy
+++ MoveLimb.groovy
@@ -25,18 +25,21 @@ if(args==null){
 
 println "Now we will move just one leg"
 DHParameterKinematics leg0 = base.getAllDHChains().get(0)
-double zLift=25
+
+double zLift=10
 println "Start from where the arm already is and move it from there with absolute location"
 TransformNR current = leg0.calcHome()
 TransformNR homereference = current.copy()
 println current
 current.translateZ(zLift);
-TransformNR ofset = current.times(homereference.inverse())
+//TransformNR ofset = current.times(homereference.inverse())
+TransformNR offset = homereference.inverse()
+TransformNR offseter = offset.times(current)
 for(double i=0;i<1;i+=0.01) {
-	TransformNR tmp = ofset.scale(i);
+	TransformNR tmp = offseter.scale(i);
 	TransformNR step = homereference.times(tmp)
 	println step
-	leg0.setDesiredTaskSpaceTransform(step,  2.0);
+	leg0.setDesiredTaskSpaceTransform(step,  1000.0);
 	ThreadUtil.wait(10)// wait for the legs to fully arrive
 }
 
